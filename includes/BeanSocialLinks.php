7633f6559b9733709a9202db89e8468e001f2395
Jon Duell <jon@jonduell.com>
2012-10-16 10:42:01 -0700

    Adds sociallinks bean

---
+++ includes/BeanSocialLinks.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ */
+
+/**
+ * Social Media Links bean.
+ *
+ * Placeholder class.  The field is applied to the entity via features.
+ */
+class BeanSocialLinks extends BeanPlugin {}
